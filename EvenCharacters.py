d7883492846e9faa10dd681fd07194c442e7f238
SuruchiSth <42490522+SuruchiSth@users.noreply.github.com>
2018-09-07 23:06:44 +0545

    Add files via upload

---
+++ EvenCharacters.py
@@ -0,0 +1,6 @@
+def evenCharacters(userInput):
+    output = userInput[::2]
+    print(output)
+string =input("Ënter a string to print even indexed characters : ")
+evenCharacters(string)
+
---
+++ OddCharacters.py
@@ -0,0 +1,5 @@
+def evenCharacters(userInput):
+    output = userInput[1::2]
+    print(output)
+string =input("Ënter a string to print odd indexed characters : ")
+evenCharacters(string)
